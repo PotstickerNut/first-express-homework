902f58ae856f1eaab7df1cfc1a2dad8bed9ad717
Janette Campbell <campbjp@hotmail.com>
2022-05-31 21:37:33 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.get("/greeting/:name", (req, res) => {
   res.send(`Hello, ${req.params.name}`);
 });
 
+app.get("/tip/:total/:percent", (req, res) => {
+  console.log(req.params.total, req.params.percent);
+  const total = Number(req.params.total);
+  const percent = Number(req.params.percent);
+
+  res.send(total * (percent / 100) + "");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
 });
